75e96d60e29627a91c103285c64ee70e6804b21d
4n70 <antonellafavorito@gmail.com>
2021-12-09 10:27:53 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/afavorito/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package afavorito.springframework.sfgpetclinic.model;
+
+/**
+ * @author A. Favorito on 09-12-2021
+ *
+ * 
+ */
+public class Pet {
+	private PetType petType;
+	private Owner ownwer;
+
+}
---
+++ src/main/java/afavorito/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package afavorito.springframework.sfgpetclinic.model;
+
+/**
+ * @author A. Favorito on 09-12-2021
+ *
+ * 
+ */
+public class PetType {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
